0715a2c82dcd56f9690cab04845ab42283174a8a
Travis Park <parktravis@icloud.com>
2021-12-07 21:25:09 -0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+
+using System;
+
+namespace Sitenine_Launcher
+{
+    public partial class MainPage : ContentPage
+    {
+        //string version;
+        //string onlineVersion;
+        //string originURL;
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            //SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+    }
+}
